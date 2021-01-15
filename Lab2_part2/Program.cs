0f8f90b5399b7e8b4e6e804fd6a1c95fceb32f23
Jason Robert <jasont.robert@gmail.com>
2021-01-14 21:35:22 -0500

    inital commit

---
+++ Lab2_part2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // YAAAAAAAAAAAAAY I'm ALL DOOOOONE
+        }
+    }
+}
